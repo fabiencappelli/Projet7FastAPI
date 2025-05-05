831642a9b9fba326406ce45a3966ff43edd97606
fabiencappelli <fabien.cappelli@gmail.com>
2025-05-05 12:11:57 +0100

    correction erreur URL API

--- start.sh
+++ start.sh
@@ -2,7 +2,8 @@
 set -e
 
 # 1) Démarrage de l'API FastAPI
-uvicorn main:app --host 0.0.0.0 --port 8000 &
-
-# 2) Démarrage de Streamlit
-streamlit run streamlit_app.py --server.address 0.0.0.0 --server.port ${PORT:-8501}
+uvicorn main:app --host 0.0.0.0 --port 8001 &
+# 2) Attends que le backend soit up
+while ! nc -z localhost 8001; do sleep 1; done
+# 23) Démarrage de Streamlit
+streamlit run streamlit_app.py --server.port ${PORT:-8000} --server.enableCORS false
