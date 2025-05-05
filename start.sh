dad30fa6cb7366fe0246c45c4037e1f4b1c25934
fabiencappelli <fabien.cappelli@gmail.com>
2025-05-05 12:31:25 +0100

    troisieme tentative

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
+
 # 1) Démarrage de l'API FastAPI
-uvicorn main:app --host 0.0.0.0 --port 8001 &
-# 2) Attends que le backend soit up
-while ! nc -z localhost 8001; do sleep 1; done
-# 23) Démarrage de Streamlit
-streamlit run streamlit_app.py --server.port ${PORT:-8000} --server.enableCORS false
+uvicorn main:app --host 0.0.0.0 --port 8000 &
+
+# 2) Démarrage de Streamlit
+streamlit run streamlit_app.py --server.address 0.0.0.0 --server.port ${PORT:-8501}
